e1155655f22e5bc808973891f5fa49a0f23bc1a0
zen <949911292@qq.com>
2022-07-03 11:41:57 +0800

    正式提交

--- Hardware/dqn.c
+++ Hardware/dqn.c
@@ -1,18 +1,30 @@
-#include "arm_math.h"
+//#include "arm_math.h"
+#include "mpu6050.h"
+#include<math.h>
 
-const int i_size=7, h_size=10, o_size=3;
-double inputs[i_size], hiddens[h_size], outputs[o_size], targets[o_size], errors[o_size];
+double learn_rate = 0.01;
+
+const int i_size=4, h_size=10, o_size=3;
+double inputs[i_size], hiddens[h_size], outputs[o_size], targets[o_size], errors1[h_size], errors2[o_size];
 double w1[i_size][h_size], w2[h_size][o_size], b1[i_size][h_size], b2[h_size][o_size];
 
-
-
+extern int 	EncoderLeft,EncoderRight;
+extern MPU6050_t MPU6050;
 
 double sigmoid(double x){
-	
+	return 1/(1+exp(-x));
 }
 
+double desigmoid(double x){
+	return sigmoid(x)*(1-sigmoid(x));
+}
+	
+	
 void get_it(){
-	;
+	inputs[0] = MPU6050.KalmanAngleX;
+	inputs[1] = MPU6050.Gx;
+	inputs[2] = EncoderLeft;
+	inputs[3] = EncoderRight;
 }
 
 void get_o(){
@@ -28,11 +40,40 @@ void get_o(){
 }
 
 void get_e(){
-	
-	
+	for (int i=0; i<o_size; i++)
+		errors2[i] = (outputs[i] - targets[i]) * desigmoid(outputs[i]);
+	for (int i=0; i<h_size; i++){
+		errors1[i] = errors2[0]*w2[i][0];
+		for (int j=1; j<o_size; j++)
+			errors1[i] += errors2[j]*w2[i][j];
+	}
+}
+
+double get_error(){
+	double ans = 0;
+	for(int i=0; i<o_size; i++)
+		ans += (outputs[i] - targets[i])*(outputs[i] - targets[i])/2;
+	return ans;
 }
 
 void learn(){
+	get_it();
+	get_o();
+	get_e();
+	for (int i=0; i<h_size; i++)
+		for (int j=0; j<o_size; j++){
+			w2[i][j] += learn_rate*errors2[j]*hiddens[i];
+			b2[i][j] += learn_rate*errors2[j];
+		}
+
+	for (int i=0; i<i_size; i++)
+		for (int j=0; j<h_size; j++){
+			w1[i][j] += learn_rate*errors1[j]*inputs[i];
+			b1[i][j] += learn_rate*errors1[j];
+		}			
+}
+
+int main(void){
 	
 }
 
